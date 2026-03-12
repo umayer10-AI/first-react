42f32aaba891cbfbe9b453dc753438e77db2eda6
umayer10-AI <mdumayer10@gmail.com>
2026-03-12 16:49:55 +0600

    add

--- src/component/section1/Page2content.jsx
+++ src/component/section1/Page2content.jsx
@@ -32,8 +32,8 @@ const Page2content = () => {
             </div>
 
         </div>
-        <div className='flex justify-center items-end'>
-            <img className='w-[80%] h-[80%] object-cover' src="https://plus.unsplash.com/premium_photo-1682309543429-6aaa6d792dae?q=80&w=1212&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <div className='flex justify-end items-end'>
+            <img className='w-[80%] h-[80%] object-cover rounded-2xl' src="https://plus.unsplash.com/premium_photo-1682309543429-6aaa6d792dae?q=80&w=1212&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
         </div>
     </div>
   )
